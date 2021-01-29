d5254e7b4f73573501d54964f1392f8d4f4df53f
Saransh <saransh0701@gmail.com>
2021-01-29 15:45:20 +0530

    Basic structure done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:camera/camera.dart';
+import 'package:cyborg/main.dart';
 import 'package:flutter/material.dart';
 
 class CyborgScreen extends StatefulWidget {
@@ -7,10 +9,35 @@ class CyborgScreen extends StatefulWidget {
 
 class _CyborgScreenState extends State<CyborgScreen> {
 
-  
+  bool isWorking = false;
+  String results = "";
+  CameraController cameraController;
+  CameraImage cameraImage;
+
+  initCamera() {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value) {
+      if(!mounted){
+        return;
+      }
+      
+      setState(() {
+        cameraController.startImageStream((image) => {
+          if(!isWorking){
+            isWorking = true,
+            cameraImage = image,
+          }
+        });
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
   }
 }
